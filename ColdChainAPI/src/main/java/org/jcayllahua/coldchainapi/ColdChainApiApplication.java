8f7a0f20a312de7c021b2dee0e77e9f761a0cada
JCayllahuaG <u202019558@upc.edu.pe>
2024-06-04 16:08:56 -0500

    project initialization

---
+++ ColdChainAPI/src/main/java/org/jcayllahua/coldchainapi/ColdChainApiApplication.java
@@ -0,0 +1,13 @@
+package org.jcayllahua.coldchainapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColdChainApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColdChainApiApplication.class, args);
+    }
+
+}
---
+++ ColdChainAPI/src/test/java/org/jcayllahua/coldchainapi/ColdChainApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jcayllahua.coldchainapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColdChainApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
